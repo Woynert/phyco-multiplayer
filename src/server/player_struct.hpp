db18f3ff0db23ccdc7563bcf5e51fec9a56cbd42
woynert <whayner.porras.2020@upb.edu.co>
2023-07-04 10:41:37 -0500

    feat(logic): Player structure

---
+++ src/server/player_struct.hpp
@@ -0,0 +1,22 @@
+#ifndef PLAYER_STRUCT_H
+#define PLAYER_STRUCT_H
+
+#include "godot_cpp/classes/e_net_connection.hpp"
+
+enum class PlayerState 
+{
+	JOINING,
+	JOINED,
+};
+
+
+struct PlayerStruct
+{
+	int slot;
+	PlayerState state;
+	godot::Ref<godot::ENetPacketPeer> peer;
+};
+
+
+
+#endif // !PLAYER_STRUCT_H
